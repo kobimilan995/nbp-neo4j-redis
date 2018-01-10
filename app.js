f4c95abcfaf824499f326ad089736f9f634d9c0d
Milan Urosevic <kobimilan995@yahoo.com>
2018-01-10 20:38:57 +0100

    admin panel, edit, update product

--- app.js
+++ app.js
@@ -15,14 +15,23 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', ""));
 var session = driver.session();
-
+//home route
 app.get('/', (req, res) => {
+	res.render('pages/index');
+});
+
+/*
+* ADMIN ROUTES
+*/
+
+//dashboard
+app.get('/admin/dashboard', (req, res) => {
 	session
 	.run('MATCH (n:Product) RETURN n LIMIT 25')
 	.then((result) => {
@@ -30,11 +39,11 @@ app.get('/', (req, res) => {
 		result.records.forEach((item) => {
 			products.push({
 				id: item._fields[0].identity.low,
-				title:item._fields[0].properties.title
+				title:item._fields[0].properties.title,
+				description:item._fields[0].properties.description
 			});
-			console.log(item._fields[0].properties);
 		});
-		res.render('pages/index', {
+		res.render('pages/admin/dashboard', {
 			products: products
 		});
 	})
@@ -42,6 +51,32 @@ app.get('/', (req, res) => {
 		console.log(error);
 	});
 });
+// end dashboard
+
+//update product
+app.post('/product/update', (req, res) => {
+	session.run("MATCH (p:Product) WHERE ID(p) = "+req.body.id+" SET p.title = '"+req.body.title+"' SET p.description= '"+req.body.description+"' RETURN p")
+	.then(result => {
+		res.redirect('/admin/dashboard');
+	})
+	.catch(error => {
+		console.log(error);
+	});
+});
+
+app.post('/product/delete', (req, res) => {
+	session.run("MATCH (p:Product) WHERE ID(p) = "+req.body.id+" DELETE p")
+	.then(result => {
+		res.redirect('/admin/dashboard');
+	})
+	.catch(error => {
+		console.log(error);
+	});
+});
+//end update product
+/*
+* END ADMIN ROUTES
+*/
 
 app.listen(3001);
 console.log('Server started on port 3000!');
