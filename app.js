4dc476924fd9b62a6bdbaea601ac4c4218be70c3
Milan Urosevic <kobimilan995@yahoo.com>
2018-01-11 02:22:09 +0100

    Dodo sam prvu relaciju. Proizvod-kateogirija. Odi na admin panel pa create product da isprobas

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var neo4j = require('neo4j-driver').v1;
+var fileUpload = require('express-fileupload');
+var moment = require('moment');
 
 
 var app = express();
@@ -17,6 +20,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(fileUpload());
 app.use(express.static(path.join(__dirname, 'public')));
 
 var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', ""));
@@ -32,19 +36,38 @@ app.get('/', (req, res) => {
 
 //dashboard
 app.get('/admin/dashboard', (req, res) => {
+	var products = [];
+	var categories = [];
 	session
-	.run('MATCH (n:Product) RETURN n LIMIT 25')
+	.run('MATCH (n:Product)-[r:BELONGS_TO]-(b) RETURN n,r,b')
 	.then((result) => {
-		var products = [];
 		result.records.forEach((item) => {
 			products.push({
 				id: item._fields[0].identity.low,
 				title:item._fields[0].properties.title,
-				description:item._fields[0].properties.description
+				description:item._fields[0].properties.description,
+				price:item._fields[0].properties.price,
+				image:item._fields[0].properties.image,
+				category:item._fields[2].properties.title
+			});
+		});
+	})
+	.catch((error) => {
+		console.log(error);
+	});
+
+	session
+	.run('MATCH (n:Category) RETURN n LIMIT 25')
+	.then((result) => {
+		result.records.forEach((item) => {
+			categories.push({
+				id: item._fields[0].identity.low,
+				title:item._fields[0].properties.title
 			});
 		});
 		res.render('pages/admin/dashboard', {
-			products: products
+			products: products,
+			categories: categories
 		});
 	})
 	.catch((error) => {
@@ -55,7 +78,17 @@ app.get('/admin/dashboard', (req, res) => {
 
 //update product
 app.post('/product/update', (req, res) => {
-	session.run("MATCH (p:Product) WHERE ID(p) = "+req.body.id+" SET p.title = '"+req.body.title+"' SET p.description= '"+req.body.description+"' RETURN p")
+	session.run("MATCH (p:Product) WHERE ID(p) = "+req.body.id+" SET p.title = '"+req.body.title+"' SET p.description= '"+req.body.description+"' SET p.price='"+req.body.price+"' RETURN p")
+	.then(result => {
+		res.redirect('/admin/dashboard');
+	})
+	.catch(error => {
+		console.log(error);
+	});
+});
+//delete product
+app.post('/product/delete', (req, res) => {
+	session.run("MATCH (p:Product) WHERE ID(p) = "+req.body.id+" DETACH DELETE p")
 	.then(result => {
 		res.redirect('/admin/dashboard');
 	})
@@ -64,16 +97,38 @@ app.post('/product/update', (req, res) => {
 	});
 });
 
-app.post('/product/delete', (req, res) => {
-	session.run("MATCH (p:Product) WHERE ID(p) = "+req.body.id+" DELETE p")
-	.then(result => {
-		res.redirect('/admin/dashboard');
-	})
-	.catch(error => {
-		console.log(error);
-	});
+//create product
+
+app.post('/product/create', (req, res) => {
+	if (!req.files)
+	    return res.status(400).send('No files were uploaded.');
+	 
+	  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+	  let sampleFile = req.files.image;
+	 
+	  // Use the mv() method to place the file somewhere on your server
+	  var newFileName = req.body.title+Date.now()+'.jpg';
+	  var newFilePath = __dirname + '/public/uploads/'+newFileName;
+	  sampleFile.mv(newFilePath, function(err) {
+	    if (err)
+	      return res.status(500).send(err);
+	  });
+	  session.run("CREATE (n:Product { title: '"+req.body.title+"', description: '"+req.body.description+"', price:'"+req.body.price+"' , image: '"+newFileName+"' }) RETURN n ")
+		.then(result => {
+			  session.run("MATCH (p:Product),(c:Category) WHERE ID(p)="+result.records[0]._fields[0].identity.low+" AND ID(c)="+req.body.category+" CREATE (p)-[r:BELONGS_TO]->(c) return r")
+				.then(result => {
+					res.redirect('/admin/dashboard');
+				})
+				.catch(error => {
+					console.log(error);
+				});
+		})
+		.catch(error => {
+			console.log(error);
+		});
+
+	
 });
-//end update product
 /*
 * END ADMIN ROUTES
 */
